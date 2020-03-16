a7b14788f5d9f1cc6f1233014ec63d937a7e7a94
Amir Moualem <amoualem@gmail.com>
2020-03-16 19:32:03 +0200

    chore: move to Typescript

--- dist/index.js
+++ dist/index.js
@@ -1,36 +1,40 @@
-const fs = require('fs');
-const needle = require('needle');
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
+const needle = require("needle");
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-
 const eventName = process.env.GITHUB_EVENT_NAME;
 if (eventName !== 'pull_request') {
-  process.exit(1);
+    process.exit(1);
 }
-
 const eventPath = process.env.GITHUB_EVENT_PATH;
-const eventData = fs.readFileSync(eventPath);
-const event = JSON.parse(eventData);
-console.log(event);
-
-const issueUrl = event.pull_request.issue_url;
-
-const url = `${issueUrl}/comments`;
-const payload = {
-  body: 'There\'s butter on my face!',
-};
-const options = {
-  json: true,
-  compressed: true,
-  headers: {
-    Authorization: 'token ' + GITHUB_TOKEN,
-  },
-};
-
-
-needle('post', url, payload, options)
-  .then((response) => {
-    console.log('huzzah!');
-  }).catch((err) => {
-    console.log('I have no idea what I\'m doing');
-  });
+if (!eventPath) {
+    console.log('Couldn\'t find the event file path.');
+    process.exit(1);
+}
+const eventData = fs.readFileSync(eventPath, 'utf8');
+const eventObj = JSON.parse(eventData);
+console.log(eventObj);
+async function postComment(issueUrl) {
+    const url = `${issueUrl}/comments`;
+    const payload = {
+        body: 'There\'s butter on my face!',
+    };
+    const options = {
+        json: true,
+        compressed: true,
+        headers: {
+            Authorization: 'token ' + GITHUB_TOKEN,
+        },
+    };
+    try {
+        await needle('post', url, payload, options);
+        console.log('huzzah!');
+    }
+    catch (err) {
+        console.log('I have no idea what I\'m doing');
+    }
+}
+const issueUrl = eventObj.pull_request.issue_url;
+postComment(issueUrl);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import * as fs from 'fs';
+import * as needle from 'needle';
+
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+
+const eventName = process.env.GITHUB_EVENT_NAME;
+if (eventName !== 'pull_request') {
+  process.exit(1);
+}
+
+const eventPath = process.env.GITHUB_EVENT_PATH;
+if (!eventPath) {
+  console.log('Couldn\'t find the event file path.');
+  process.exit(1);
+}
+
+const eventData = fs.readFileSync(eventPath, 'utf8');
+const eventObj = JSON.parse(eventData);
+console.log(eventObj);
+
+async function postComment(issueUrl: string): Promise<void> {
+  const url = `${issueUrl}/comments`;
+  const payload = {
+    body: 'There\'s butter on my face!',
+  };
+  const options = {
+    json: true,
+    compressed: true,
+    headers: {
+      Authorization: 'token ' + GITHUB_TOKEN,
+    },
+  };
+
+  try {
+    await needle('post', url, payload, options);
+    console.log('huzzah!');
+  } catch (err) {
+    console.log('I have no idea what I\'m doing');
+  }
+}
+
+const issueUrl = eventObj.pull_request.issue_url;
+postComment(issueUrl);
