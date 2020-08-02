14ee8ec49c86122ea6818902d2eebf80e167f974
Amir Moualem <amoualem@gmail.com>
2020-08-02 12:59:43 +0300

    fix: add more error handling per commit
    if we fail to process a commit title, log it and the error that caused it.

--- src/commit.ts
+++ src/commit.ts
@@ -36,37 +36,41 @@ function processCommits(commitHeaders: string[]): ICommitData {
   };
 
   for (const commitHeader of commitHeaders) {
-    const line = commitHeader.trim();
-    console.log('processing line:', line);
-    const words = line.split(' ');
-
-    if (words.length <= 1) {
-      console.log('missing commit message, not processing it');
-      continue;
-    }
-
-    if (words[1] === 'Merge') { // hack
-      console.log('treating line as a merge commit, not processing it');
-      continue;
-    }
-
-    if (!words[1].endsWith(':')) {
-      console.log('unknown prefix', words[1], 'treating it as misc');
-      processedCommits['others'].push(line);
-      continue;
-    }
-
-    const hash = words[0];
-    const prefix = words[1].split(':')[0];
-    const rest = words.slice(2).join(' ');
-
-    const lineDescription = `${rest} (${hash})`;
-    if (prefix in processedCommits) {
-      console.log(`adding "${lineDescription}" to ${prefix}`);
-      processedCommits[prefix].push(lineDescription);
-    } else {
-      console.log(`adding "${lineDescription}" to others`);
-      processedCommits['others'].push(lineDescription);
+    try {
+      const line = commitHeader.trim();
+      console.log('processing line:', line);
+      const words = line.split(' ');
+  
+      if (words.length <= 1) {
+        console.log('missing commit message, not processing it');
+        continue;
+      }
+  
+      if (words[1] === 'Merge') { // hack
+        console.log('treating line as a merge commit, not processing it');
+        continue;
+      }
+  
+      if (!words[1].endsWith(':')) {
+        console.log('unknown prefix', words[1], 'treating it as misc');
+        processedCommits['others'].push(line);
+        continue;
+      }
+  
+      const hash = words[0];
+      const prefix = words[1].split(':')[0];
+      const rest = words.slice(2).join(' ');
+  
+      const lineDescription = `${rest} (${hash})`;
+      if (prefix in processedCommits) {
+        console.log(`adding "${lineDescription}" to ${prefix}`);
+        processedCommits[prefix].push(lineDescription);
+      } else {
+        console.log(`adding "${lineDescription}" to others`);
+        processedCommits['others'].push(lineDescription);
+      }
+    } catch (err) {
+      console.log(`couldn't handle commit title ${commitHeader}: ${err}`);
     }
   }
 
